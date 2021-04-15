55561ad9b6fc1d5519224e8760d1f909b5e3cb7e
Maged <magedd977@gmail.com>
2021-04-15 23:15:20 +0200

    before changing SearchPage

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,13 +1,27 @@
 import React from "react";
 import * as BooksAPI from "./BooksAPI";
 import "./App.css";
+const shelfNameMapper = {
+  currentlyReading: "Currently Reading",
+  wantToRead: "Want To Read",
+  read: "Read",
+};
 
 class BookShelf extends React.Component {
+  /* onChangeHandler = (event) => {
+        setLoading();
+        BooksAPI.update(book, event.target.value)
+          .then(fetchBooks)
+          .catch((err) => console.error("error updating book", err))
+          .finally(clearLoading);
+      }
+     */
+
   render() {
-    const { books, shelf } = this.props;
+    const { books, shelf, refPage } = this.props;
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{shelf}</h2>
+        <h2 className="bookshelf-title">{shelfNameMapper[shelf]}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
             {books.map((book) => {
@@ -26,7 +40,16 @@ class BookShelf extends React.Component {
                         }}
                       />
                       <div className="book-shelf-changer">
-                        <select>
+                        <select
+                          value={shelf}
+                          onChange={(event) => {
+                            BooksAPI.update(book, event.target.value)
+                              .then(refPage)
+                              .catch((err) =>
+                                console.error("error updating book", err)
+                              );
+                          }}
+                        >
                           <option value="move" disabled>
                             Move to...
                           </option>
@@ -35,7 +58,7 @@ class BookShelf extends React.Component {
                           </option>
                           <option value="wantToRead">Want to Read</option>
                           <option value="read">Read</option>
-                          <option value="none">None</option>
+                          <option value="None">None</option>
                         </select>
                       </div>
                     </div>
